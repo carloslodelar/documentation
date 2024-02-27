031deb4c9d5bf28a7fa6e059fa641597cb5bbb9d
carloslodelar <43219654+carloslodelar@users.noreply.github.com>
2024-02-26 21:50:48 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/cardano.ico',
 
   // Set the production url of your site here
-  url: 'carloslodelar.github.io/documentation/',
+  url: 'https://carloslodelar.github.io/documentation/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentation/',
